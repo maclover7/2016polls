bb62345c460cde43f0dcdb743c316aff487f3a4f
maclover7 <me@jonathanmoss.me>
2015-09-16 17:48:29 -0400

    Fetch data from Huff Post

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+get 'httparty'
+gem 'json'
 gem 'rake'
 gem 'rubocop'
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,9 +1,41 @@
-require 'sinatra'
 require 'erb'
+require 'httparty'
+require 'json'
+require 'sinatra'
 
 # The app!
 class Pollster < Sinatra::Base
+  # Fetch data from the Huffington Post
+  class HuffAPI
+    def self.fetch_huff_data(party)
+      huff_chart_url = 'http://elections.huffingtonpost.com/pollster/api/charts'
+      url = huff_chart_url + "/2016-national-#{party}-primary"
+      data = ::HTTParty.get(url).body
+      ::JSON.load(data)
+    end
+
+    #-----
+
+    def self.leader(party)
+      data = fetch_huff_data(party)
+      data['estimates'].first['choice']
+    end
+
+    def self.standings(party)
+      huff_data = fetch_huff_data(party)
+      data = {}
+      huff_data['estimates'].collect do |candidate|
+        data[candidate['choice']] = candidate['value']
+      end
+      data
+    end
+  end
+
   get '/' do
+    @democratic_leader = HuffAPI.leader('democratic')
+    @democratic_standings = HuffAPI.standings('democratic')
+    @republican_leader = HuffAPI.leader('gop')
+    @republican_standings = HuffAPI.standings('gop')
     erb :home
   end
 end
